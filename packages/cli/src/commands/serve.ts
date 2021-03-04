892732eebf0de3348a33d08dc4f991de6dffe42e
GustavLinden <fintvader@hotmail.com>
2021-03-04 13:23:00 +0100

    aster

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { Command } from 'commander';
-import { serve } from '@gnotebook/local-api';
+import { serve } from '@glnotebook/local-api';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
